34e7434f69848cc00066666ae49b9f654591f09f
yairg <yjgarizabalo@gmail.com>
2024-10-31 07:01:08 -0500

    [task] add config routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,19 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "ng-task-18-61b33", "appId": "1:449582625876:web:7aa4d03562d4ff247469c1", "storageBucket": "ng-task-18-61b33.appspot.com", "apiKey": "AIzaSyClih3bBWM64RjY6tYiXFESLkCmp8nybyo", "authDomain": "ng-task-18-61b33.firebaseapp.com", "messagingSenderId": "449582625876" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        "projectId": "ng-task-18-61b33",
+        "appId": "1:449582625876:web:7aa4d03562d4ff247469c1",
+        "storageBucket": "ng-task-18-61b33.appspot.com",
+        "apiKey": "AIzaSyClih3bBWM64RjY6tYiXFESLkCmp8nybyo",
+        "authDomain": "ng-task-18-61b33.firebaseapp.com",
+        "messagingSenderId": "449582625876"
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())]
 };
